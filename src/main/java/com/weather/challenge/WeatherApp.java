615607c04cc2c98d8815e7a45b2f0ca4ca991046
Guillermo Grillo <guillermo.grillo88@gmail.com>
2017-09-28 15:44:45 -0300

    actualizo servicio a yahoo weather

--- src/main/java/com/weather/challenge/WeatherApp.java
+++ src/main/java/com/weather/challenge/WeatherApp.java
@@ -1,9 +1,5 @@
 package com.weather.challenge;
 
-import com.github.fedy2.weather.YahooWeatherService;
-import com.github.fedy2.weather.data.Channel;
-import com.github.fedy2.weather.data.Forecast;
-import com.github.fedy2.weather.data.unit.DegreeUnit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/weather/challenge/service/YahooService.java
+++ src/main/java/com/weather/challenge/service/YahooService.java
@@ -1,19 +1,47 @@
 package com.weather.challenge.service;
 
-import com.weather.challenge.dto.external.Weather;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Collections;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URLEncoder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.weather.challenge.dto.external.Forecast;
+import com.weather.challenge.dto.external.Item;
+import com.weather.challenge.dto.external.Weather;
 
 @Service
 public class YahooService {
 
     public Weather findWeatherByWoeid(int woeid) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", Weather.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        String url = "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid="+woeid+"&u=c";
+        try{
+        	ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        	String resp = exchange.getBody();
+        	ObjectMapper o = new ObjectMapper();
+        	Item item = o.readValue(resp, Item.class);
+        	for (Forecast forecast : item.getForecasts()) {
+				System.out.println("Fecha:"+forecast.getDate());
+				System.out.println("Min:"+forecast.getLow());
+				System.out.println("Max:"+forecast.getHigh());
+				System.out.println("Estado:"+forecast.getText());
+			}
+        	System.out.println(item.getTitle());
+        }catch(Exception e){
+        	System.out.println(e.getMessage());
+        }
+        return null;
     }
 
 }
